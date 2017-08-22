57d679e8d4a8437b6391b8d675171a7daf000703
TheCodeor <447690182@qq.com>
2017-08-22 16:35:01 +0800

    Initial Commit

---
+++ GoodsDetailDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoodsDetailDemo
+//
+//  Created by fanpeng on 2017/8/22.
+//  Copyright © 2017年 fanpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoodsDetailDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GoodsDetailDemo
+//
+//  Created by fanpeng on 2017/8/22.
+//  Copyright © 2017年 fanpeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
